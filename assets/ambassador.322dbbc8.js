b0233f07773aeb6d4d1b95d2e34b94f170d35e50
alexandervu <alexandervu@users.noreply.github.com>
2021-08-06 11:11:11 +0000

    deploy: 0890231fb8c10a754c4b572c7fda59fb519577bf

---
+++ assets/ambassador.322dbbc8.js
@@ -0,0 +1,1 @@
+import{d as a,b as n,o as r,i as d}from"./vendor.140c162d.js";var e=a({name:"Admin"});e.render=function(a,d,e,m,o,s){return r(),n("div",null," admin page here ")},d(e).mount("#ambassador");
